a9572e492d408d5a0bdfa356ce9592e22fe98f07
TankiFlo <59439336+TankiFlo@users.noreply.github.com>
2025-05-23 22:04:21 +0200

    Update run_scraper.cmd

--- run_scraper.cmd
+++ run_scraper.cmd
@@ -1,8 +1,7 @@
-echo Reminder: Sind alle Team URL Datein up-to-date?
-pause
 cd scripts
 python logoscraper_prm.py
 python logoscraper_opl.py
 python logoscraper_toornament.py
+
 echo Scraping fertig! (ob erfolgreich oder nicht, schaue in die obigen Nachrichten)
 pause
