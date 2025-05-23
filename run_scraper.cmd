d2cc4d4227ae7af73de78f83c961e06c4627caef
TankiFlo <florian.kleint@gmx.de>
2025-05-23 14:56:52 +0200

    Finished OPL Logoscraper
    Finished OPL Logoscraper + added to run_scraper.cmd

--- run_scraper.cmd
+++ run_scraper.cmd
@@ -1,4 +1,7 @@
+echo Reminder: Sind alle Team URL Datein up-to-date?
+pause
 cd scripts
 python logoscraper_prm.py
+python logoscraper_opl.py
 echo Scraping fertig! (ob erfolgreich oder nicht, schaue in die obigen Nachrichten)
 pause
--- scripts/logoscraper_opl.py
+++ scripts/logoscraper_opl.py
@@ -6,6 +6,9 @@ import os # for creating the needed directory
 path = "../logos/LoL/OPL/"
 ''' path for the logos '''
 
+download_url = "https://www.opleague.pro/screenshot?url=https%3A%2F%2Fwww.opleague.pro%2Fstyles%2Fmedia%2Fteam%2F<TEAM-ID>%2FLogo_100.webp"
+'''Replace <TEAM-ID> with the team id of the wanted team'''
+
 def download_image(url, name):
     res = requests.get(url, stream=True)
     if res.status_code == 200:
@@ -20,17 +23,11 @@ def scrape_upcoming_matches(url):
     soup = BeautifulSoup(response.text, 'html.parser')
     upcoming_match_div = soup.find(class_ = 'match-and-event-table__content-container')
     if upcoming_match_div is not None:
-        for div in upcoming_match_div.find_all(class_ = 'match-and-event-table__content-item paginator_0_data_class'):
-            scrape_match_page(div.find('a')['href'])
-
-
-def scrape_match_page(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find_all('a')
-    for link in links:
-        print(link.get('href'))
-    print(soup.find('main'))
+        for div in upcoming_match_div.find_all(class_ = 'img-text'):
+            teamId = div.find('img')['src'].split('/')[6]
+            url = download_url.replace('<TEAM-ID>', teamId)
+            name = (div.find('img')['alt'] + ".png").replace('/', '') # replace() for sanitization
+            download_image(url, name)
 
 if __name__ == '__main__':
     try: # create needed directory
@@ -44,7 +41,6 @@ if __name__ == '__main__':
         print(f"An error occurred: {e}")
 
     # start scraping
-#    with open('../team_urls/team_urls_opl.txt', 'r') as f: # team_urls_opl.txt contains our current PRM teams (they need to be the current teams from the current split as relevant information isn't present on normal team pages)
-#        for line in f:
-#            scrape_upcoming_matches(line)
-    download_image('https://www.opleague.pro/styles/media/team/5404/Logo_30.webp?1698268749', 'test.webp')
+    with open('../team_urls/team_urls_opl.txt', 'r') as f: # team_urls_opl.txt contains our current OPL teams
+        for line in f:
+            scrape_upcoming_matches(line.strip())
